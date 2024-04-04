b963d99ab01996fb536f776ee1afc57f357d1d6c
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2024-04-04 21:46:50 +0100

    tmux and wsl

--- wezterm.lua
+++ wezterm.lua
@@ -6,6 +6,13 @@ local mux = wezterm.mux
 -- This table will hold the configuration.
 local config = wezterm.config_builder()
 
+config.wsl_domains = {
+    {
+        name = "WSL:Ubuntu-22.04",
+        distribution = "Ubuntu-22.04",
+        default_prog = { "tmux" },
+    }
+}
 config.default_domain = 'WSL:Ubuntu-22.04'
 -- This will hold the configuration.
 
@@ -15,14 +22,14 @@ config.font_size = 17
 
 -- Setting the background image and its opacity
 config.background = {
-	{
-		source = { File = "C:/Users/Nuno Ramos/Pictures/Wallpapers/ocean.png" },
-	},
-	{
-		source = { Color = "rgba(0,0,0,0.5)" },
-		width = "100%",
-		height = "100%",
-	},
+    {
+        source = { File = "C:/Users/Nuno Ramos/Pictures/Wallpapers/ocean.png" },
+    },
+    {
+        source = { Color = "rgba(0,0,0,0.5)" },
+        width = "100%",
+        height = "100%",
+    },
 }
 
 -- config.color_scheme = 'AdventureTime'
@@ -33,44 +40,49 @@ config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.window_padding = {
-	left = 20,
-	right = 20,
-	top = 35,
-	bottom = 15,
+    left = 20,
+    right = 20,
+    top = 35,
+    bottom = 15,
 }
 
 local launch_menu = {}
 table.insert(launch_menu, {
-	label = "Pwsh",
-	args = { "-NoLogo" },
+    label = "Pwsh",
+    args = { "-NoLogo" },
 })
 
 config.launch_menu = launch_menu
 
 --Remember size
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
 end)
 
+
 config.keys = {
-	{
-		key = "v",
-		mods = "CTRL",
-		action = wezterm.action.PasteFrom("Clipboard"),
-	},
-	{
-		key = "t",
-		mods = "SHIFT|ALT",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{ key = "h", mods = "SHIFT|ALT", action = act.ActivateTabRelative(-1) },
-	{ key = "l", mods = "SHIFT|ALT", action = act.ActivateTabRelative(1) },
-	{
-		key = "d",
-		mods = "SHIFT|ALT",
-		action = wezterm.action.CloseCurrentTab({ confirm = true }),
-	},
+    {
+        key = "v",
+        mods = "CTRL",
+        action = wezterm.action.PasteFrom("Clipboard"),
+    },
+    {
+        key = "t",
+        mods = "SHIFT|ALT",
+        action = act.SpawnTab("CurrentPaneDomain"),
+    },
+    { key = "h", mods = "SHIFT|ALT", action = act.ActivateTabRelative(-1) },
+    { key = "l", mods = "SHIFT|ALT", action = act.ActivateTabRelative(1) },
+    {
+        key = "d",
+        mods = "SHIFT|ALT",
+        action = wezterm.action.CloseCurrentTab({ confirm = true }),
+    },
+    -- Show launcher for workspaces
+    { key = 'o', mods = 'SHIFT|ALT', action = wezterm.action.ShowLauncherArgs {
+        flags = "FUZZY|WORKSPACES",
+    }},
 }
 
 return config
